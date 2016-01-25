9c329fa0d4825f967f9b1884b1f78ce10a50dd49
shahsaurabh0605 <shahsaurabh0605@gmail.com>
2016-01-25 22:52:11 +0530

    My gem

---
+++ lib/novice.rb
@@ -0,0 +1,14 @@
+class Novice
+  def self.head
+    "RUBY PROGRAMMING LANGUAGE"
+  end
+
+  def self.paraone
+     "Ruby is a dynamic, reflective, object-oriented, general-purpose programming language. It was designed and developed in the mid-1990s by Yukihiro (Matz) Matsumoto in Japan."
+  end
+
+  def self.paratwo
+      "According to its creator, Ruby was influenced by Perl, Smalltalk, Eiffel, Ada, and Lisp. It supports multiple programming paradigms, including functional, object-oriented, and imperative. It also has a dynamic type system and automatic memory management."
+  end
+
+end
---
+++ novice.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+s.name = "novice"
+s.version = '0.0.1'
+s.date = '2016-01-24'
+s.authors = ["Saurabh Shah "]
+s.email = ["shahsaurabh0103@gmail.com"]
+s.summary = "My own gem"
+s.description = "Ruby Programming"
+s.homepage = "https://github.com/shahsaurabh0605/Novice.git"
+#s.files = ["lib/mygem3.rb"]
+# or
+s.files = Dir["{lib}/**/*.rb", "bin/*", "LICENSE", "*.md"]
+end
